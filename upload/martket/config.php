36778add828a46f562273a7c90443f01e780855e
WealthTech <wealthtech@ManhNH.NTA>
2026-01-16 11:19:59 +0700

    update env

--- upload/martket/config.php
+++ upload/martket/config.php
@@ -1,6 +1,6 @@
 <?php
 // APPLICATION
-define('APPLICATION', 'martket');
+define('APPLICATION', 'admin');
 
 // HTTP
 define('HTTP_SERVER', 'http://martket.ap-southeast-1.elasticbeanstalk.com/martket/');
